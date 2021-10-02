5fb977883ecdc35044a58720e9e2c46ce0f796f8
Aditya Paul <76274584+adityapaul-1234@users.noreply.github.com>
2021-10-02 12:21:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+rover_x = 10;
+rover_y = 10;
+rover_width = 100;
+rover_height = 90;
+
+function add(){
+    background_imgTag = new Image();
+    background_imgTag.onload = uploadBackground;
+    background_imgTag.src = "mars.jpg";
+
+    rover_imgTag = new Image();
+    rover_imgTag.onload = uploadRover;
+    rover_imgTag.src = "rover.png";
+}
+
+function uploadBackground(){
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadRover(){
+    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+}
